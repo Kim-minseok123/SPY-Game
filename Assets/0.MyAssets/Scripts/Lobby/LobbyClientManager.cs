088138d706e163607ea721a62de1085948b53a16
Bedford <qmal789@dgu.ac.kr>
2022-12-22 18:01:38 +0900

    SPY-Game Start

---
+++ Assets/0.MyAssets/Scripts/Lobby/LobbyClientManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyClientManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BlackPannel blackPannel = BlackPannel.instance;
+        StartCoroutine(blackPannel.FadeOut());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/0.MyAssets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        BlackPannel blackPannel = BlackPannel.instance;
+        StartCoroutine(blackPannel.FadeOut());
+        StartCoroutine(Next());
+    }
+    public IEnumerator Next() {
+        yield return new WaitForSeconds(2f);
+        BlackPannel blackPannel = BlackPannel.instance;
+        yield return StartCoroutine(blackPannel.FadeIn());
+        blackPannel.NextScene("Lobby");
+    }
+
+}
