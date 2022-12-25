d6ecdcec81e13d9f8d4ecfbe89c81791371b8313
Bedford <qmal789@dgu.ac.kr>
2022-12-25 20:55:44 +0900

    Room Join and GameStart

--- Assets/0.MyAssets/Scripts/Server/LobbyServerManager.cs
+++ Assets/0.MyAssets/Scripts/Server/LobbyServerManager.cs
@@ -13,6 +13,8 @@ public class LobbyServerManager : MonoBehaviourPunCallbacks
     public GameObject AfterObject;
     public GameObject BeforeObject;
     public TMP_InputField RoomNameInput;
+    public TextMeshProUGUI PlayerList;
+    public TextMeshProUGUI RoomInfo;
     int RoomName = 10000000;
     
     void Awake()=> Screen.SetResolution(540, 960, false);
@@ -52,16 +54,15 @@ public class LobbyServerManager : MonoBehaviourPunCallbacks
 
     }
 
-    //public void JoinRoom() => PhotonNetwork.JoinRoom(roomInput.text);
-
-    //public void JoinOrCreateRoom() => PhotonNetwork.JoinOrCreateRoom(roomInput.text, new RoomOptions { MaxPlayers = 2 }, null);
-
     public void JoinRoom() {
-        if (RoomNameInput.text.Equals("")) { print("방 이름 없음"); return; }
         PhotonNetwork.JoinRoom(RoomNameInput.text); 
     }
 
-    public void LeaveRoom() => PhotonNetwork.LeaveRoom();
+    public void LeaveRoom() { 
+        PhotonNetwork.LeaveRoom();
+        LobbyClientManager.instanse.RoomUIOff();
+        AfterObject.SetActive(true);
+    }
 
     public override void OnCreatedRoom()
     {
@@ -74,35 +75,36 @@ public class LobbyServerManager : MonoBehaviourPunCallbacks
         AfterObject.SetActive(false);
         LobbyClientManager.instanse.JoinRoomUIOff();
         LobbyClientManager.instanse.RoomUIOn();
+        Info();
         print("방참가완료"); 
     }
-
-    public override void OnCreateRoomFailed(short returnCode, string message) => print("방만들기실패");
-
+    public override void OnPlayerEnteredRoom(Player newPlayer) => Info();
+    public override void OnPlayerLeftRoom(Player otherPlayer) => Info();
+    public override void OnCreateRoomFailed(short returnCode, string message) => CreateRoom();
     public override void OnJoinRoomFailed(short returnCode, string message) => print("방참가실패");
-
-    public override void OnJoinRandomFailed(short returnCode, string message) => print("방랜덤참가실패");
-
-    [ContextMenu("정보")]
     void Info()
     {
         if (PhotonNetwork.InRoom)
         {
-            print("현재 방 이름 : " + PhotonNetwork.CurrentRoom.Name);
-            print("현재 방 인원수 : " + PhotonNetwork.CurrentRoom.PlayerCount);
-            print("현재 방 최대인원수 : " + PhotonNetwork.CurrentRoom.MaxPlayers);
+            PlayerList.text = "";
+            RoomInfo.text = "방 이름 : \n" + PhotonNetwork.CurrentRoom.Name + "\n\n\n\n\n\n\n\n\n" 
+                + PhotonNetwork.CurrentRoom.PlayerCount + "명 / 최대 " 
+                + PhotonNetwork.CurrentRoom.MaxPlayers +"명";
 
-            string playerStr = "방에 있는 플레이어 목록 : ";
-            for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++) playerStr += PhotonNetwork.PlayerList[i].NickName + ", ";
-            print(playerStr);
+            for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+                PlayerList.text += PhotonNetwork.PlayerList[i].NickName + "\n";
+
         }
-        else
-        {
-            print("접속한 인원 수 : " + PhotonNetwork.CountOfPlayers);
-            print("방 개수 : " + PhotonNetwork.CountOfRooms);
-            print("모든 방에 있는 인원 수 : " + PhotonNetwork.CountOfPlayersInRooms);
-            print("로비에 있는지? : " + PhotonNetwork.InLobby);
-            print("연결됐는지? : " + PhotonNetwork.IsConnected);
-        }
+    }
+
+    public void GameStart() {
+        if (PhotonNetwork.CurrentRoom.PlayerCount < 4) { print("인원이 너무 적습니다.");  return; }
+        StartCoroutine(GameMapStart());
+    }
+    public IEnumerator GameMapStart()
+    {
+        BlackPannel blackPannel = BlackPannel.instance;
+        yield return StartCoroutine(blackPannel.FadeIn());
+        //게임 씬 이동
     }
 }
